0840fedcade6bd749f0e38d844671a3d431dc5fa
Reza Mousavi <rza.mousavi@gmail.com>
2018-07-05 13:36:03 +0200

    Reading config from a secret files, which is more dynamic.

--- docker-rest/config/script/startup.sh
+++ docker-rest/config/script/startup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+role_id_file="/vault/secret/.${CONFD_APPROLE}_role_id"
+if [ -f "$role_id_file" ]
+then
+    echo "'${role_id_file}' found. Overriding environment variable value with file content"
+    export CONFD_ROLE_ID=$(cat "${role_id_file}")
+fi
+secret_id_file="/vault/secret/.${CONFD_APPROLE}_secret_id"
+if [ -f "$secret_id_file" ]
+then
+    echo "'${secret_id_file}' found. Overriding environment variable value with file content"
+	export CONFD_SECRET_ID=$(cat "${secret_id_file}")
+fi
+echo "CONFD_ROLE_ID : '$CONFD_ROLE_ID'"
+echo "CONFD_SECRET_ID : '$CONFD_SECRET_ID'"
 echo "Substituting environment variables for : /etc/confd/confd.toml.unprocessed"
 envsubst < /etc/confd/confd.toml.unprocessed > /etc/confd/confd.toml
 for entry in /etc/confd/conf.d/*
